1b645e232d3fcb55fdf5ad6eacd3af0de0b7e744
Songchao Wang <songchaow@outlook.com>
2020-09-14 10:07:08 +0800

    Implement makegroup tool.

--- makegroup.py
+++ makegroup.py
@@ -21,24 +21,49 @@ if filename == '':
 
 # 打开表格，实施分组
 grade_wb = load_workbook(filename)
-main_ws = grade_wb.active
+main_ws = grade_wb['All']
 if len(grade_wb.worksheets):
     other_sheets = grade_wb.worksheets[1::]
     for s in other_sheets:
         grade_wb.remove(s)
 
 NUM_GROUP = 5
+NUM_ROW_HEADER = 7
 
 def copy_sheet_header(main_ws:Worksheet, dst_ws:Worksheet):
     # determine max column
     #dst_ws.merge_cells('A1:M7')
-    for r in main_ws.iter_rows(1, 7):
+    dst_ws.merged_cells.ranges = main_ws.merged_cells.ranges
+    for r in main_ws.iter_rows(1, NUM_ROW_HEADER):
         for c in r:
-            dst_ws.cell(c.row, c.column, c.value)
-            # dst_ws.cell
+            #dst_ws.cell(c.row, c.column, c.value)
+            dst_ws.copy_cell(c.row, c.column, c)
 
+sub_group_sheets = []
+# sub_group_sheet_end_idx = []
+sub_group_sheet_stu_num = []
 
-sub_ws = grade_wb.create_sheet('WSC')
-copy_sheet_header(main_ws, sub_ws)
+for group_idx in range(NUM_GROUP):
+    sheet_name = chr(ord('A') + group_idx) + '组'
+    sub_ws = grade_wb.create_sheet(sheet_name)
+    copy_sheet_header(main_ws, sub_ws)
+    sub_group_sheets.append(sub_ws)
+    #sub_group_sheet_end_idx.append(NUM_ROW_HEADER + 1)
+    sub_group_sheet_stu_num.append(0)
+
+for r in main_ws.iter_rows(NUM_ROW_HEADER + 1, 300):
+    # fetch stu id in this class
+    if r[0].value is None:
+        continue
+    remains = (int(r[0].value)-1) % NUM_GROUP
+    for c in r:
+        dst_ws = sub_group_sheets[remains]
+        next_idx = NUM_ROW_HEADER + 1 + sub_group_sheet_stu_num[remains]
+        dst_ws.copy_cell_to_row(c.row, c.column, c, next_idx)
+    sub_group_sheet_stu_num[remains] += 1
+
 
 grade_wb.save('test.xlsx')
+print('分组后的表格已保存。各组的人数为：')
+for i in range(NUM_GROUP):
+    print(sub_group_sheet_stu_num[i])
